9699dadf5ed1396ab8fc3a994b86804d8e0d7a4a
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2023-09-01 00:46:37 +0200

    Add overflow example

--- examples/src/example.cpp
+++ examples/src/example.cpp
@@ -8,14 +8,15 @@ int main() {
 
     const auto str = nanofmt::format<"Signed: {:05}\n"
                                      "Unsigned: {:4}\n"
-                                     "Float: {:08}">(145, -54, -123.4);
+                                     "Float: {:08.3}">(145, -54, -123.4);
 
     /// User defined literal syntax
 
     using namespace nanofmt::literals;
 
     const auto str2 = "Binary: 0b{:08b}\n"
-                      "Hexadecimal: 0x{:04x}"_fmt(123, 0xfa);
+                      "Hexadecimal: 0x{:04x}\n"
+                      "Overflow: {:4}"_fmt(123, 0xfa, 12345);
 
     std::cout << str.data() << std::endl;
     std::cout << str2.data() << std::endl;
